7d6f2d0a03a7de57241229d9f0e56f4dc2126cf4
GoyaChen <46504384+GoyaChen@users.noreply.github.com>
2022-06-09 07:52:51 -0500

    test omit

--- src/Wraper/Filter.js
+++ src/Wraper/Filter.js
@@ -1,11 +1,14 @@
 import {Button} from "@mui/material";
+import _ from "underscore";
 
 export default function Filter(props) {
     function handleClick(){
-        props.setData({
-            ...props.data,
-            value: ['value1', 'value2']
-        })
+        let newData = {...props.data}// [...array]
+        newData = _.omit(newData, "aaa")
+        newData.name = ["newName1", "newName2"]
+        props.setData(newData)
+        // props.data.name = ["newName3", "newName4"]
+        // props.setData(props.data)
     }
     return (
         <Button onClick={handleClick}>
--- src/Wraper/Wraper.js
+++ src/Wraper/Wraper.js
@@ -1,14 +1,20 @@
-import Filter from "../Filter/Filter";
+// import Filter from "../Filter/Filter";
+import Filter from "./Filter";
 import {useState} from "react";
 import Scatter from "../viz/Scatter";
 import {Container} from "@mui/material";
+import Result from "./Result";
 
 export default function Wraper() {
-    const [data, setData] = useState({})
+    const [data, setData] = useState({
+        name:["name1", "name2"],
+        aaa:["bbb", "ccc"]
+    })
     return (
         <Container>
             <Filter setData={setData} data={data}/>
-            <Scatter data={data} setData={setData}/>
+            <Result data={data} setData={setData}/>
+            {/*<Scatter data={data} setData={setData}/>*/}
         </Container>
     )
 }
